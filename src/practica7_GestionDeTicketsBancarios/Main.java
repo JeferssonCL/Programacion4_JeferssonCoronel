5f3b87d77f8303534331489a2b23aa0597e24e36
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-08-10 13:16:54 -0400

    fix: Update Repo

--- src/practica7_GestionDeTicketsBancarios/Main.java
+++
@@ -1,8 +0,0 @@
-package practica7_GestionDeTicketsBancarios;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        BankTicketsManager bncBank = new BankTicketsManager();
-        bncBank.runProgram();
-    }
-}
