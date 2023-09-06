8af35d04831246a7d08673470fa5c54cc489f330
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-09-06 13:29:21 -0400

    finish task

--- src/Practica11_Threads2/App.java
+++ src/Practica11_Threads2/App.java
@@ -1,24 +1,41 @@
 package Practica11_Threads2;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        int n = 10;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
         int[] inputArray = new int[n];
         int[] outputArray = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            inputArray[i] = i + 1;
-        }
+        fillInputArray(inputArray, n);
 
         ExecutorService executor = Executors.newFixedThreadPool(n);
         CompletionService<Integer> completionService = new ExecutorCompletionService<>(executor);
 
+        submitTasks(completionService, inputArray, outputArray);
+
+        waitForTasksToComplete(completionService, n);
+
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        printOutputArray(outputArray);
+    }
+
+    private static void fillInputArray(int[] inputArray, int n) {
         for (int i = 0; i < n; i++) {
+            inputArray[i] = i + 1;
+        }
+    }
+
+    private static void submitTasks(CompletionService<Integer> completionService, int[] inputArray, int[] outputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
             final int index = i;
             completionService.submit(() -> {
                 int result = SequenceTwoMultiplier.getInstance().doMultiplicationSequence(inputArray[index]);
@@ -26,7 +43,9 @@ public class App {
                 return result;
             });
         }
+    }
 
+    private static void waitForTasksToComplete(CompletionService<Integer> completionService, int n) throws InterruptedException, ExecutionException {
         Random random = new Random();
         int completedTasks = 0;
 
@@ -41,14 +60,13 @@ public class App {
             double progress = (double) completedTasks / n * 100;
             System.out.println("Progreso: " + progress + "% - Hilo " + completedTasks + " terminado - Resultado: " + result);
         }
+    }
 
-        executor.shutdown();
-        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
+    private static void printOutputArray(int[] outputArray) {
         System.out.print("Resultado: [");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < outputArray.length; i++) {
             System.out.print(outputArray[i]);
-            if (i < n - 1)
+            if (i < outputArray.length - 1)
                 System.out.print(", ");
         }
         System.out.println("]");
